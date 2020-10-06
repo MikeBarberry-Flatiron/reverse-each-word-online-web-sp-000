8f7850edd6b7e317f53eb992861782c860adb64e
Mike Barberry <mikebarberry@protonmail.com>
2020-10-06 18:04:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,5 @@ puts reverse_each_word("I am a good boy").typeof
 
 
 # change string into array
-# iterate through array reversing each word  
-# return the array back into a string 
+# iterate through array reversing each word
+# return the array back into a string
